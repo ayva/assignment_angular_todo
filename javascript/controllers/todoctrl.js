c874bccd83be31c18e556ed5d3b653f63a6588da
Nick Sarlo <nicholas.l.sarlo@gmail.com>
2015-09-15 12:58:10 -0400

    CRUD working

--- javascript/controllers/todoctrl.js
+++ javascript/controllers/todoctrl.js
@@ -1,5 +1,5 @@
 app.controller('TodoCtrl', ['$scope', '$window', function($scope, $window){
-  
+
   $scope.item = { text: "Get groceries from the store",
                 dueDate: new Date(),
                 completed: false };
@@ -7,9 +7,22 @@ app.controller('TodoCtrl', ['$scope', '$window', function($scope, $window){
   $scope.items = [{text: "Get Groceries from the store", dueDate: "Sep 7, 2016", completed: false},{text: "Get Groceries from the store", dueDate: "Sep 7, 2016", completed: false}];
   $scope.createTodo = function(){
     $scope.items.push({text: $scope.taskText, dueDate: $scope.taskDate, completed: false});
-    $scope.taskText="";
-    $scope.dueDate=undefined;
+    $scope.taskText=null;
+    $scope.dueDate=null;
     $window.alert("Task created");
   };
+  $scope.clearCompleted = function(){
+    for (var i=0; i < $scope.items.length; i++){
+      if ($scope.items[i].completed){
+        $scope.items.splice(i, 1)
+      }
+    }
+  };
+  $scope.showCompleted = function(){
+
+  }
+  $scope.deleteTodo = function(index){
+    $scope.items.splice(index, 1)
+  }
 }
 ]);
